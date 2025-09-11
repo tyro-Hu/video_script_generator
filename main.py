6db43a8d7bf9518576819a4b74da13f56f0b91da
hujiankang <78073038+tyro-Hu@users.noreply.github.com>
2025-09-11 15:43:39 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+import streamlit as st
+from utils import generate_script
+
+st.title("🎬视频脚本生成器")
+
+# 添加侧边栏
+with st.sidebar:
+    deepseek_api_key = st.text_input("请输入你的DeepSeek API密钥", type="password")
+    st.markdown("[获取DeepSeek API密钥](https://platform.deepseek.com/api_keys)")
+
+subject = st.text_input("💡请输入视频的主题")
+video_length = st.number_input("🕒请输入视频的大致时长（单位：分钟）", min_value=0.1, step=0.1, value=1.0)
+creavity = st.slider("✨请输入视频脚本的创造力（数字小说明更严谨，数字大说明更多样）", min_value=0.0, max_value=1.0, step=0.1, value=0.2)
+
+submit = st.button("🚀生成脚本")
+
+# 检查密钥
+if submit and not deepseek_api_key:
+    st.info("请输入你的DeepSeek API密钥")
+    st.stop()
+
+# 检查主题
+if submit and not subject:
+    st.info("请输入视频的主题")
+    st.stop()
+
+# 检查时长
+if submit and not video_length >= 0.1:
+    st.info("视频长度需要大于或等于0.1")
+    st.stop()
+
+if submit:
+    # 生成脚本
+    with st.spinner(("AI正在思考中，请稍等...")):
+        wiki_result, title, script = generate_script(subject, video_length, creavity, deepseek_api_key)
+    st.success("视频脚本已生成！")
+
+    st.subheader("🔥标题：")
+    st.write(title)
+    st.subheader("📝视频脚本：")
+    st.write(script)
+    with st.expander("📚维基百科搜索结果👀"):
+        st.info(wiki_result)
---
+++ utils.py
@@ -0,0 +1,50 @@
+from langchain.prompts import ChatPromptTemplate
+from langchain_deepseek import ChatDeepSeek
+from dotenv import load_dotenv
+import os
+from langchain_community.utilities import WikipediaAPIWrapper
+
+# 加载环境变量
+load_dotenv()
+DEEPSEEK_API_KEY = os.getenv("DEEPSEEK_API_KEY")
+
+
+def generate_script(subject, video_length, creativity, api_key):
+
+    title_template = ChatPromptTemplate.from_messages(
+        [
+            ("human", "请为'{subject}'这个主题的视频想一个吸引人的标题。只需要输出标题即可，不要输出其他内容。")
+        ]
+    )
+
+    script_template = ChatPromptTemplate.from_messages(
+        [
+            ("human", 
+            """
+            你是一位短视频频道的博主。根据以下标题和相关信息，为短视频频道写一个视频脚本。
+            视频标题：{title}，视频时长：{duration}分钟，生成的脚本的长度尽量遵循视频时长的要求。
+            要求开头抓住眼球，中间提供干货内容，结尾有惊喜，脚本格式也请按照【开头、中间、结尾】分割。
+            整体内容的表达方式要尽量轻松有趣，吸引年轻人。
+            脚本内容可以结合以下维基百科搜索出的信息，但仅作为参考，只结合相关的即可，对不相关的进行忽略：
+            ```{wikipedia_search}```
+            """)
+        ]
+    )
+
+    model = ChatDeepSeek(model="deepseek-chat", api_key=api_key, temperature=creativity)
+
+    title_chain = title_template | model
+    script_chain = script_template | model
+
+    title = title_chain.invoke({"subject" : subject}).content
+
+    search = WikipediaAPIWrapper(lang="zh")
+    search_result = search.run(subject)
+
+    script = script_chain.invoke({"title" : title, "duration" : video_length, "wikipedia_search" : search_result}).content
+
+    return search_result, title, script
+
+# wiki_result, title, script = generate_script("sora模型", 1, 0.7, DEEPSEEK_API_KEY)
+
+# print(script)
